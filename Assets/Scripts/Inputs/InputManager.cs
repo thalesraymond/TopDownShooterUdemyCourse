dc90a2cf6b3f1c140f3b5d3029bbb2e133bcf7fa
Thales Raymond <32554150+thalesraymond@users.noreply.github.com>
2025-01-29 19:46:55 -0300

    project setup

---
+++ Assets/Scripts/Inputs/InputManager.cs
@@ -0,0 +1,58 @@
+﻿using UnityEngine;
+
+namespace Inputs
+{
+    public class InputManager : MonoBehaviour
+    {
+        public static InputManager Instance { get; private set; }
+        public Controls Controls { get; private set; }
+    
+        public Vector2 PlayerMovementValue { get; private set; }
+        public bool IsTryingToMove { get; private set; }
+        
+        public Vector2 PlayerAimValue { get; private set; }
+
+        private void Awake()
+        {
+            if (Instance == null)
+            {
+                Instance = this;
+            }
+            else
+            {
+                Destroy(this);
+            }
+
+            this.Controls = new Controls();
+        }
+
+        private void OnEnable()
+        {
+            this.Controls.Enable();
+        }
+    
+        private void OnDisable()
+        {
+            this.Controls.Disable();
+        }
+
+        private void Update()
+        {
+            this.HandleMovement();
+            
+            this.HandleAim();
+        }
+
+        private void HandleMovement()
+        {
+            this.PlayerMovementValue = this.Controls.Player.Movement.ReadValue<Vector2>();
+        
+            this.IsTryingToMove = this.PlayerMovementValue != Vector2.zero;
+        }
+        
+        private void HandleAim()
+        {
+            this.PlayerAimValue = this.Controls.Player.Aim.ReadValue<Vector2>();
+        }
+    }
+}
